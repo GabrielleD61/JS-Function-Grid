c9a36039a3d62543dd8a54765d70f2ede50e094f
Gabrielle Deleon <gdeleon.exol@gmail.com>
2017-01-17 21:44:27 +0000

    added a smile

--- script.js
+++ script.js
@@ -1,1 +1,27 @@
-//Write your function calls below this line
+/*
+setColor(3, 2, "blue");
+setColor(3, 6, "blue");
+setColor(5, 1, "blue");
+setColor(6, 2, "blue");
+setColor(7, 3, "blue");
+setColor(7, 4, "blue");
+setColor(7, 5, "blue");
+setColor(6, 6, "blue");
+setColor(5, 7, "blue");
+*/
+
+setColor(2, 6, "red")
+setColor(1, 5, "red")
+setColor(1, 4, "red")
+setColor(1, 3, "red")
+setColor(2, 2, "red")
+setColor(3, 2, "red")
+setColor(4, 3, "red")
+setColor(5, 4, "red")
+setColor(6, 5, "red")
+setColor(7, 6, "red")
+setColor(8, 5, "red")
+setColor(8, 4, "red")
+setColor(8, 3, "red")
+setColor(7, 2, "red") 
+
